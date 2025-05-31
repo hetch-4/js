f5416e83141383cdad8249cd2c8ceb9e6b21b1e8
hetch-4 <humphreydola@gmail.com>
2025-05-31 09:05:40 -0400

    password generator

---
+++ projects/passwordGenerator.js
@@ -0,0 +1,23 @@
+const generatePassword = len =>{
+  let passwordgen = '';
+  let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()';
+  let array = [];
+  
+  let i = 0;
+  do{
+    let random = Math.floor(Math.random() * chars.length);
+    //console.log(chars[random])
+    array.push(chars[random])
+    
+    i++;
+  }while(i < len);
+
+  passwordgen = array.join('');
+  //console.log(password);
+  return passwordgen;
+}
+
+//console.log(generatePassword(10));
+let num = 8;
+const password =generatePassword(8)
+console.log(`Generated password:${password}`);
