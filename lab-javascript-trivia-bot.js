d9bbaf6407fdf5f54ba9c4194b31aebfebec8d59
hetch-4 <humphreydola@gmail.com>
2025-07-31 23:19:43 +0300

    inital commit

---
+++ lab-javascript-trivia-bot.js
@@ -0,0 +1,23 @@
+** start of script.js **
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Fact bot";
+let botLocation = "Minesota";
+let favoriteLanguage = "JavaScript";
+
+console.log("My name is "+botName+" and I live on "+ botLocation+".");
+console.log("My favorite programming language is "+favoriteLanguage+".");
+
+let codingFact = "You can use "+favoriteLanguage+" in both backend and frontend.";
+console.log(codingFact);
+codingFact = favoriteLanguage +" is a scripting language and is interprated";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + "is an interprated language.";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - "+botName+" from "+botLocation+".");
+
+** end of script.js **
+
