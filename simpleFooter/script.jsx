34ebe6f2efda49801c8764363e7a9c9370c6c455
Parrot Live user <user@localhost.localdomain>
2025-08-06 20:48:27 +0000

    reusable footer

---
+++ simpleFooter/script.jsx
@@ -0,0 +1,22 @@
+export const Footer = () => {
+    return  (
+      <footer>
+        <ul>
+          <li>Fitness Dashboard</li>
+          <li>Services</li>
+        </ul>
+        <ul>
+          <li>Medium</li>
+          <li>Discord</li>
+        </ul>
+        <ul>
+          <li>Privacy Policy</li>
+          <li>Terms and Conditions</li>
+        </ul>
+        <p>&copy;Fitness Dashboard. All rights reserved.</p>
+        <a href="#">Home</a>
+        <a href="#">About us</a>
+        <a href="#">Contact us</a>
+      </footer>
+    );
+  };
