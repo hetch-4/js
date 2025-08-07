7db1bac542ec04dd939012a1598e98282b2a0260
Parrot Live user <user@localhost.localdomain>
2025-08-07 20:29:22 +0000

    strings

---
+++ strings.js
@@ -0,0 +1,26 @@
+let message = "Hello, World!";
+let greeting = message.slice(0, 5);
+//getting part of a string
+console.log(greeting);
+
+greeting = message.slice(7);
+//getting part of a string from a starting point 7
+console.log(greeting);
+
+let upperGreeting = message.toUpperCase();
+//converting a string to uppercase //all characters are changed
+console.log(upperGreeting);
+
+let shout = 'I A LEARNING JAVASCRIPT';
+//converting a string to lowercase
+console.log(shout.toLowerCase());
+
+let text = "I love javascript";
+console.log(text);
+//replacing a string within a string ie javascript - coding 
+console.log(text.replace('javascript', 'coding'));
+
+let phrase = 'Hello, world! Welcome to the world of coding';
+//replace() function replaces the first occurence only
+console.log(phrase);
+console.log(phrase.replace('world','universe'));
