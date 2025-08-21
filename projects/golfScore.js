3b890e9bf16d296c64b7e71e34fa35b627606f5a
hetch-4 <humphreydola@gmail.com>
2025-08-21 11:53:50 +0000

    Golf score translator

--- projects/golfScore.js
+++ projects/golfScore.js
@@ -2,18 +2,30 @@ const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey"
 
 const golfScore = (par,strokes) => {
   if(strokes == 1){
-    return 'Hole-in-one!';
+    return names[0];
   }else if(strokes<=par-2){
-    return 'Eagle';
+    return names[1];
   }else if(strokes==par-1){
-    return 'Birdie';
+    return names[2];
   }else if(strokes===par){
-    return 'Par';
+    return names[3];
   }else if(strokes == par+1){
-    return 'Bogey';
+    return names[4];
   }else if(strokes == par+2){
-    return 'Double Bogey';
+    return names[5];
   }else if(strokes >= par+2){
-    return 'Go Home!';
+    return names[6];
   }
 }
+
+console.log(golfScore(4, 1));
+console.log(golfScore(4, 2));
+console.log(golfScore(5, 2));
+console.log(golfScore(4, 3));
+console.log(golfScore(4, 4));
+console.log(golfScore(1, 1));
+console.log(golfScore(5, 5));
+console.log(golfScore(4, 5));
+console.log(golfScore(4, 6));
+console.log(golfScore(4, 7));
+console.log(golfScore(5, 9));
