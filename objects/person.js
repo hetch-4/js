df9fbf2d97323cec4cbf42b1a8bd19e38d47552f
hetch-4 <humphreydola@gmail.com>
2025-08-27 12:10:15 +0000

    objects

---
+++ objects/person.js
@@ -0,0 +1,18 @@
+const person = {
+  name: 'Alice',
+  age: 30,
+  city: 'New York'
+};
+
+//above is an object person with 3 properties name, age, city
+
+//how to acces object properties
+console.log(person.age);
+console.log(person.name);
+
+console.log(person['name']);
+console.log(person['age']);
+
+//bracket allow to use varibles
+let propertyName = "city";
+console.log(person[propertyName]); 
