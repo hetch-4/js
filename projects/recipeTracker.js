c66d89f9a327b7b69e7ff1267f34d73d340bc94e
Jeph Humphrey <155738167+hetch-4@users.noreply.github.com>
2025-08-28 09:42:50 +0000

    Create recipeTracker.js
    recipe tracker

---
+++ projects/recipeTracker.js
@@ -0,0 +1,12 @@
+const recipes = [];
+
+const recipe1 = {
+  name: 'Spaghetti Carbonara',
+  ingredients: ['spaghetti', 'Parmesan cheese', 'pancetta', 'black pepper'],
+  cookingTime: 22,
+  totalIngredients: null,
+  difficultyLevel: '',
+  ratings: [4, 5, 4, 5],
+  averageRating: null,
+};
+
