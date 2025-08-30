1f84e0d1590d54033576e8919ad7b21aa994940d
hetch-4 <humphreydola@gmail.com>
2025-08-30 10:09:13 +0000

    recipe tracker

--- projects/recipeTracker.js
+++ projects/recipeTracker.js
@@ -10,3 +10,71 @@ const recipe1 = {
   averageRating: null,
 };
 
+const recipe2 = {
+  name: 'Chicken Curry',
+  ingredients: ['chicken breast', 'coconut milk', 'curry powder', 'onion', 'garlic'],
+  cookingTime: 42,
+  totalIngredients: null,
+  difficultyLevel: '',
+  ratings: [4, 5, 5, 5],
+  averageRating: null,
+};
+
+const recipe3 = {
+  name: 'Vegetable Stir Fry',
+  ingredients: ['broccoli', 'carrot', 'bell pepper'],
+  cookingTime: 15,
+  totalIngredients: null,
+  difficultyLevel: '',
+  ratings: [4, 3, 4, 5],
+  averageRating: null,
+};
+
+recipes.push(recipe1, recipe2, recipe3);
+
+const getAverageRating=(rating)=>{
+  let sum = 0;
+  for(let i=0; i<rating.length; i++){
+    sum += rating[i];
+  }
+  return sum/rating.length;
+}
+console.log(getAverageRating([2,3,4,4,5]));
+
+
+function getTotalIngredients(ingredients) {
+  return ingredients.length;
+}
+
+function getDifficultyLevel(cookingTime) {
+  if (cookingTime <= 30) {
+    return 'easy';
+  } else if (cookingTime <= 60) {
+    return 'medium';
+  } else {
+    return 'hard';
+  }
+}
+
+const recipe1AverageRating = getAverageRating(recipe1.ratings);
+console.log(recipe1AverageRating);
+
+const recipe1TotalIngredients = getTotalIngredients(recipe1.ingredients);
+console.log(recipe1TotalIngredients);
+
+const recipe1DifficultyLevel = getDifficultyLevel(recipe1.cookingTime);
+console.log(recipe1DifficultyLevel);
+
+recipe1.averageRating = getAverageRating(recipe1.ratings);
+recipe1.totalIngredients = getTotalIngredients(recipe1.ingredients)
+recipe1.difficultyLevel = getDifficultyLevel(recipe1.cookingTime)
+
+recipe2.averageRating = getAverageRating(recipe2.ratings);
+recipe2.totalIngredients = getTotalIngredients(recipe2.ingredients);
+recipe2.difficultyLevel = getDifficultyLevel(recipe2.cookingTime);
+
+recipe3.averageRating = getAverageRating(recipe3.ratings);
+recipe3.totalIngredients = getTotalIngredients(recipe3.ingredients)
+recipe3.difficultyLevel = getDifficultyLevel(recipe3.cookingTime)
+
+console.log(recipes);
