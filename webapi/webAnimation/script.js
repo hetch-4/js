07db8f401bfab38269b8876b03355a97bd7c971b
hetch-4 <humphreydola@gmail.com>
2025-10-16 05:49:41 -0400

    web animations

---
+++ webapi/webAnimation/script.js
@@ -0,0 +1,30 @@
+const square = document.querySelector("#square");
+const playBtn = document.querySelector("#playBtn");
+const pauseBtn = document.querySelector("#pauseBtn");
+
+const animation = square.animate(
+    [{transform:"translateX(0px)"},{ transform: "translateX(500px)"}],
+    {
+        duration: 2000,
+        iteration: Infinity,
+        direction: "alternate",
+        easing: "ease-in-out"
+    }
+);
+
+// set the onfinish property to log a message when the animation 
+animation.onfinish=()=>{
+    console.log("Animation finished!");
+};
+
+//play animation when the play button is clicked
+AnimationPlaybackEvent.addEventListener("click",()=>{
+    animation.play();
+    console.log("you start the animation");
+});
+
+//pause the animation on button pause
+pauseBtn.addEventListener("click", ()=>{
+    animation.pause();
+    console.log("You pause the animation");
+});
