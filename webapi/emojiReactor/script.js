efcc7db822b7cfea21462bea000fd41641364925
hetch-4 <humphreydola@gmail.com>
2025-10-21 04:56:32 -0400

    emoji reactor

--- webapi/emojiReactor/script.js
+++ webapi/emojiReactor/script.js
@@ -1,25 +1,15 @@
-const happyBtn = document.querySelector("#happy-btn");
+function updateCount(btn) {
+  const countEl = btn.querySelector(".count");
+  let currCount = +countEl.textContent.split("/")[0];
+  
+  if (currCount === 10) return;
+  
+  currCount++;
+  
+  countEl.textContent = `${currCount}/10`;
+}
 
-
-  const countEl = happyBtn.querySelector(".count");
-
-  let strings = countEl //+ tochange string to num
-            .textContent //get string value of count element
-            .split("");//split the string to && get element @ index 0
-
-  let num = +strings[0];
-happyBtn.addEventListener("click", () => {
-  strings.shift();
-  //console.log(countEl.textContent);
-  //console.log(num )
-  strings.unshift(num);
-  //console.log(strings.join(''))
-  //countEl.textContent = num.join('',rest);
-  if(num >10){
-    return;
-  }else{
-    num ++;
-  }
-  countEl.textContent = strings.join('');
+const btns = document.querySelectorAll(".emoji-btn");
+btns.forEach((btn)=>{
+  btn.addEventListener("click",()=>updateCount(btn));
 })
-
