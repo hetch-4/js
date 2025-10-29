dadd2e8b0a701cc2c3d39526644d338864985ab5
hetch-4 <humphreydola@gmail.com>
2025-10-29 02:32:18 -0400

    intro to node.js

---
+++ nodeJS/env.js
@@ -0,0 +1,4 @@
+console.log('Environment:', process.env.NODE_ENV || 'development');
+console.log('Custom variable:' process.env.MY_VARIABLE);
+console.log('Database URL:' process.env.DATABASE_URL || 'Not set');
+
