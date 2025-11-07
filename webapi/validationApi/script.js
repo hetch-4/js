bcad5a6c159803308d5921d86b0460b5af86a0cf
hetch-4 <humphreydola@gmail.com>
2025-11-07 08:56:10 -0500

    validation apis

--- webapi/validationApi/script.js
+++ webapi/validationApi/script.js
@@ -1,6 +1,22 @@
-function myFunction() {
+function checkValidity() {
     const inpObj = document.getElementById("id1");
     if(!inpObj.checkValidity()){
         document.getElementById("demo").innerHTML = inpObj.validationMessage;
     }
+}
+
+function checkOverFlow(){
+    let text = "value Ok";
+    if(document.getElementById("id2").validity.rangeOverflow){
+        text = "value too Large";
+    }
+    document.getElementById('demo2').innerHTML = text
+}
+
+function checkUnderFlow(){
+    let text = "Value Ok";
+    if(document.getElementById("id3").validity.rangeUnderflow){
+        text = "Value too small";
+    }
+    document.getElementById('demo3').innerHTML = text;
 }
