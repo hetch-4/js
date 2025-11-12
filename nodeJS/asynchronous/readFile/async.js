23e668c2daf644bb1434d7451c631526bf47c737
hetch-4 <humphreydola@gmail.com>
2025-11-12 02:21:09 -0500

    initial commit

--- nodeJS/asynchronous/readFile/async.js
+++ nodeJS/asynchronous/readFile/async.js
@@ -2,10 +2,8 @@ const fs = require('fs');
 
 console.log('1. Starting async read...');
 fs.readFile('myfile.txt', 'utf8', (err, data) => {
-  if (err) throw err;
+  //if (err) throw err;
   console.log('2. File contents:', data);
 });
 
 console.log('3. Done starting read operation');
-
-            
